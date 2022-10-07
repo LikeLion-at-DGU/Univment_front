5ae1c6a826d8a894558776644a3614fd56b9b911
AsherOh <102159721+AsherOh@users.noreply.github.com>
2022-10-07 23:47:30 +0900

    build App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import "./App.css";
+import AppRouter from "./components/AppRouter";
 
 function App() {
-  return <div className="App">Hi</div>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <div className="App">
+      <AppRouter isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/AppRouter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Router } from "react-router-dom";
+
+const AppRouter = () => {
+  return (
+    <div>
+      <Router></Router>
+    </div>
+  );
+};
+
+export default AppRouter;
