b0e3a25670a39500278381c89415cafd6ee3ccf8
AsherOh <102159721+AsherOh@users.noreply.github.com>
2022-10-10 16:03:15 +0900

    add nav setting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 import AppRouter from "./components/AppRouter";
 
 function App() {
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "../static/css/Header.module.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className={styles.headerBody}>
+        <p className={styles.text}>UNIVMENT</p>
+      </div>
+    </>
+  );
+};
+
+export default Header;
