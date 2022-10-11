bfa13d9def9c05f3059dbb5c7739626c73a795e3
AsherOh <102159721+AsherOh@users.noreply.github.com>
2022-10-11 17:41:45 +0900

    add landingPage yes or no button

--- src/pages/InitialPage.js
+++ src/pages/InitialPage.js
@@ -2,17 +2,44 @@ import React from "react";
 import Header from "../components/Header";
 import styles from "../static/css/InitialPage.module.css";
 import ReactTypingEffect from "react-typing-effect";
+import { Button } from "@mui/material";
+import { border, width } from "@mui/system";
 
 const InitialPage = () => {
   return (
     <>
       <Header />
-      <div className={styles.textWrap}>
-        <ReactTypingEffect
-          text={[" '오늘 기록하고 싶은 일이 있었나요?' "]}
-          cursorRenderer={(cursor) => <h3>{cursor}</h3>}
-          className={styles.mainText}
-        />
+      <div className={styles.container}>
+        <section className={styles.textWrap}>
+          <ReactTypingEffect
+            text={[" '오늘 기록하고 싶은 일이 있었나요?' "]}
+            cursorRenderer={(cursor) => <h3>{cursor}</h3>}
+          />
+        </section>
+        <section className={styles.btnWrap}>
+          <Button
+            variant="outlined"
+            sx={{
+              color: "#383b3d",
+              border: "1px solid #383b3d",
+              width: "10vw",
+              fontFamily: "Jeju Myeongjo",
+            }}
+          >
+            네
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              color: "#383b3d",
+              border: "1px solid #383b3d",
+              width: "10vw",
+              fontFamily: "Jeju Myeongjo",
+            }}
+          >
+            아니오
+          </Button>
+        </section>
       </div>
     </>
   );
