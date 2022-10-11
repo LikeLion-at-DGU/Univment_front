83f36cd8484e38cb8c982483cda76cf59e5cd49d
AsherOh <102159721+AsherOh@users.noreply.github.com>
2022-10-11 17:00:15 +0900

    add react typing component API

--- src/pages/InitialPage.js
+++ src/pages/InitialPage.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Header from "../components/Header";
 import styles from "../static/css/InitialPage.module.css";
+import ReactTypingEffect from "react-typing-effect";
 
 const InitialPage = () => {
   return (
     <>
       <Header />
-      <div className={styles.mainText} id="mainText"></div>
+      <div className={styles.textWrap}>
+        <ReactTypingEffect
+          text={[" '오늘 기록하고 싶은 일이 있었나요?' "]}
+          cursorRenderer={(cursor) => <h3>{cursor}</h3>}
+          className={styles.mainText}
+        />
+      </div>
     </>
   );
 };
