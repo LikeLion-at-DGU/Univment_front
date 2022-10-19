978c7d7aa81a6ba44c5891a0ba574264ba7e46da
AsherOh <102159721+AsherOh@users.noreply.github.com>
2022-10-20 02:35:17 +0900

    id, email, password validation setting

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import {
   Button,
   CssBaseline,
   TextField,
   FormControl,
-  FormControlLabel,
-  Checkbox,
   FormHelperText,
   Grid,
   Box,
@@ -32,30 +30,67 @@ const SignUp = () => {
     id: "",
     email: "",
     password: "",
-    rePassword: "",
-    checked: false,
+    passwordConfirm: "",
   });
+  const { id, email, password, passwordConfirm } = inputs;
 
   // 인풋 에러 관리
   const [errors, setErrors] = useState({
     idError: "",
     emailError: "",
     passwordError: "",
-    rePasswordError: "",
-    registerError: "",
+    passwordConfirmError: "",
   });
 
-  // 회원가입 동의 체크박스 관리
-  const handleAgree = (e) => {
-    setInputs({
-      ...inputs,
-      checked: e.target.checked,
-    });
-  };
+  // 유효성 검사
+  const [validation, setValidation] = useState({
+    isId: false,
+    isEmail: false,
+    isPassword: false,
+    isPasswordConfirm: false,
+  });
+
+  // Id 유효성 관리
+  const onChangeId = useCallback(
+    (e) => {
+      const idRegex = /^[a-zA-Z\\d`~!@#$%^&*()-_=+]{4,20}$/;
+      if (!idRegex.test(id) || id.length < 1)
+        setErrors({
+          ...errors,
+          idError:
+            "숫자, 알파벳 및 숫자와 관련된 특수문자 조합의 올바른 아이디를 입력해주세요. 글자수 제한[4~20]",
+        });
+      else
+        setErrors({
+          ...errors,
+          idError: "",
+        });
+      const { value, name } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value,
+      });
+    },
+    [id]
+  );
 
   // Boxs Form 버튼 클릭 시 실행
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const data = new FormData(e.currentTarget);
+    const joinData = {
+      id: data.get("id"),
+      email: data.get("email"),
+      password: data.get("password"),
+      rePassword: data.get("rePassword"),
+    };
+    const { id, email, password, rePassword } = joinData;
+    console.log(joinData);
+  };
+
+  const onPost = async (data) => {
+    console.log("회원가입 포스트 정상 신청", data);
   };
 
   return (
@@ -90,6 +125,7 @@ const SignUp = () => {
                     id="id"
                     name="id"
                     label="아이디"
+                    onChange={onChangeId}
                     error={errors.idError !== "" || false}
                   />
                 </Grid>
@@ -114,10 +150,10 @@ const SignUp = () => {
                     id="password"
                     name="password"
                     label="비밀번호 (숫자+영문자+특수문자 8자리 이상)"
-                    error={inputs.password !== "" || false}
+                    error={errors.passwordState !== "" || false}
                   />
                 </Grid>
-                <FormHelperTexts>{inputs.password}</FormHelperTexts>
+                <FormHelperTexts>{errors.passwordState}</FormHelperTexts>
                 <Grid item xs={12}>
                   <TextField
                     required
@@ -130,14 +166,14 @@ const SignUp = () => {
                   />
                 </Grid>
                 <FormHelperTexts>{errors.passwordError}</FormHelperTexts>
-                <Grid item xs={12}>
+                {/* <Grid item xs={12}>
                   <FormControlLabel
                     control={
                       <Checkbox onChange={handleAgree} color="primary" />
                     }
                     label="Univment 회원가입에 동의합니다."
                   />
-                </Grid>
+                </Grid> */}
               </Grid>
               <Button
                 type="submit"
